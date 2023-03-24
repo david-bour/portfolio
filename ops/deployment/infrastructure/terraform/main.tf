f8f1e011cee542542bf796dc2f31aadfdd70a64a
David Bour <dbour@Davids-MacBook-Pro.local>
2023-03-24 10:17:30 -0700

    Pin terraform version

--- ops/deployment/infrastructure/terraform/main.tf
+++ ops/deployment/infrastructure/terraform/main.tf
@@ -1,25 +1,25 @@
 terraform {
-    backend "gcs" {
-        bucket = "cloud-resume-tf-state"
-        prefix = "terraform/state"
+  backend "gcs" {
+    bucket = "cloud-resume-tf-state"
+    prefix = "terraform/state"
+  }
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.58.0"
     }
-    required_providers {
-      google = {
-        source  = "hashicorp/google"
-        version = "4.58.0"
-      }
-    }
-    
+  }
+
 }
 
 provider "google" {
-    project = "cloudresume-380001"
+  project = "cloudresume-380001"
 }
 
 resource "google_storage_bucket" "cloud-resume-site" {
-    name          = "cloud-resume-website"
-    location      = "US"
-    force_destroy = true
+  name          = "cloud-resume-website"
+  location      = "US"
+  force_destroy = true
 
-    uniform_bucket_level_access = true
+  uniform_bucket_level_access = true
 }
