83aeaa8d2efd0c230d5f46b37ef817f81f2e5475
David Bour <dbour@Davids-MacBook-Pro.local>
2023-04-25 22:07:43 -0700

    config

--- ops/deployment/infrastructure/terraform/main.tf
+++ ops/deployment/infrastructure/terraform/main.tf
@@ -115,7 +115,7 @@ resource "google_api_gateway_api" "api_cfg" {
 resource "google_api_gateway_api_config" "api_cfg" {
   provider      = google-beta
   api           = google_api_gateway_api.api_cfg.api_id
-  api_config_id = "visitor-api-gateway-cfg"
+  api_config_id = "visitor-api-gateway-cfg-1"
 
   openapi_documents {
     document {
