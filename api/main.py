62ca1b21ad55e6138b2b6cc89b15d8353f27ac92
David Bour <dbour@Davids-MacBook-Pro.local>
2023-05-14 21:30:45 -0700

    add cors

--- api/main.py
+++ api/main.py
@@ -1,7 +1,25 @@
+import os
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from firebase_db import gcp_firebase
 
 app = FastAPI()
+
+origins = [
+    'https://davidbour.com',
+    'https://www.davidbour.com',
+    'https://api.davidbour.com'
+]
+if os.environ.get('DEV', None):
+    origins.append('http://localhost')
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def main():
