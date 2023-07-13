0cd373fb037625faaae31b3718329e14a260af75
david bour <dbour.developer@gmail.com>
2023-07-12 19:55:46 -0700

    - chore: fix fmt

--- ops/deployment/infrastructure/terraform/k8s-vpc.tf
+++ ops/deployment/infrastructure/terraform/k8s-vpc.tf
@@ -1,19 +1,19 @@
 variable "project_id" {
-    description = "project id"
+  description = "project id"
 }
 
 variable "region" {
-    description = "region"
+  description = "region"
 }
 
 resource "google_compute_network" "vpc" {
-    name                    = "${var.project_id}-vpc"
-    auto_create_subnetworks = "false"
+  name                    = "${var.project_id}-vpc"
+  auto_create_subnetworks = "false"
 }
 
 resource "google_compute_subnetwork" "subnet" {
-    name          = "${var.project_id}-subnet"
-    region        = var.region
-    network       = google_compute_network.vpc.name
-    ip_cidr_range = "10.10.0.0/24"
+  name          = "${var.project_id}-subnet"
+  region        = var.region
+  network       = google_compute_network.vpc.name
+  ip_cidr_range = "10.10.0.0/24"
 }
