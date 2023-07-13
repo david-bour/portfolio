071eb2ecf6e055ec7f4628eb2c4728776ea9bf44
david bour <dbour.developer@gmail.com>
2023-07-12 19:57:37 -0700

    - fix: missing number of nodes block

--- ops/deployment/infrastructure/terraform/k8s-gke.tf
+++ ops/deployment/infrastructure/terraform/k8s-gke.tf
@@ -8,6 +8,11 @@ variable "gke_password" {
   description = "number of gke nodes"
 }
 
+variable "gke_num_nodes" {
+    default     = 2
+    description = "number of gke nodes"
+}
+
 resource "google_container_cluster" "primary" {
   name     = "${var.project_id}-gke"
   location = var.region
