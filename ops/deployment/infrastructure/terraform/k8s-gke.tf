78b68af8662106f5400c1e8ccd46c3a2991e9318
david bour <dbour.developer@gmail.com>
2023-07-19 18:44:04 -0700

    - chore: use zones to prevent going over limit

--- ops/deployment/infrastructure/terraform/k8s-gke.tf
+++ ops/deployment/infrastructure/terraform/k8s-gke.tf
@@ -15,7 +15,7 @@ variable "gke_num_nodes" {
 
 resource "google_container_cluster" "primary" {
   name     = "${var.project_id}-gke"
-  location = var.region
+  location = var.zone
 
   remove_default_node_pool = true
   initial_node_count       = 1
@@ -30,7 +30,7 @@ resource "google_container_cluster" "primary" {
 
 resource "google_container_node_pool" "primary_nodes" {
   name       = google_container_cluster.primary.name
-  location   = var.region
+  location   = var.zone
   cluster    = google_container_cluster.primary.name
   node_count = var.gke_num_nodes
 
--- ops/deployment/infrastructure/terraform/terraform.tfvars
+++ ops/deployment/infrastructure/terraform/terraform.tfvars
@@ -1,2 +1,2 @@
 project_id = "cloudops-388321"
-region     = "us-central1"
+zone       = "us-central1-f"
